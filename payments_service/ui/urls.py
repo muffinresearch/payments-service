ecf05005374e3389cf6fac56c06ebe18fef76d91
Stuart Colville <scolville@mozilla.com>
2015-05-07 15:47:43 +0100

    Add payment form to ui app

--- payments_service/ui/urls.py
+++ payments_service/ui/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = patterns(
     '',
     url(r'^$', views.index, name='index'),
+    url(r'^pay/$', views.payment_form, name='payment_form'),
 )
--- payments_service/ui/views.py
+++ payments_service/ui/views.py
@@ -5,3 +5,9 @@ from django.conf import settings
 def index(request):
     """Page that serves the HTML for the payments-ui"""
     return render(request, 'base.html', {'static_path': settings.UI_STATIC})
+
+
+def payment_form(request):
+    """Page that serves the HTML for the payments-ui"""
+    return render(request, 'payment-form.html',
+                  {'static_path': settings.UI_STATIC})
